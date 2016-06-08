29872133b17c24af3f2c39505d4d563787aa6fb4
Phil Pilon <pilon24@icloud.com>
2016-06-08 03:31:08 -0400

    Adds authorization for the users controller

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -54,12 +54,13 @@ describe Api::V1::UsersController do
     end
   end
   describe "DELETE #destroy" do
-  before(:each) do
-    @user = FactoryGirl.create :user
-    delete :destroy, { id: @user.id }, format: :json
+    before(:each) do
+      @user = FactoryGirl.create :user
+      api_authorization_header @user.auth_token #we added this line
+      delete :destroy, id: @user.auth_token
+    end
+
+    it { should respond_with 204 }
+
   end
-
-  it { should respond_with 204 }
-
-end
 end
--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -3,5 +3,8 @@ module Requests
     def json
       @json ||= JSON.parse(response.body)
     end
+    def api_authorization_header(token)
+      request.headers['Authorization'] =  token
+    end
   end
 end  
