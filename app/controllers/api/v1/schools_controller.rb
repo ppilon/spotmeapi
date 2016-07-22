5ab60f50a5da3eb0f8d94dfb6015a625e703694f
Phil Pilon <pilon24@icloud.com>
2016-07-22 14:26:25 -0400

    Added create action to schools controller

--- app/controllers/api/v1/schools_controller.rb
+++ app/controllers/api/v1/schools_controller.rb
@@ -3,4 +3,18 @@ class Api::V1::SchoolsController < ApplicationController
 	def show
 		respond_with School.find(params[:id])
 	end
+	def create
+		school = School.new(school_params)
+
+		if school.save
+			render json: school, status: 201, location: [:api, school]
+		else
+			render json: { errors: school.errors }, status: 422
+		end
+	end
+	private
+
+	def school_params
+		params.require(:school).permit(:school_name, :latitude, :longitude, :radius, admins_attributes: [:email, :password, :password_confirmation])
+	end
 end
