1976e4cb20c8e4ab1a9147eeafa219cd5133fb81
Phil Pilon <pilon24@icloud.com>
2016-08-26 13:57:54 -0400

    Users can create alerts

--- app/controllers/api/v1/alerts_controller.rb
+++ app/controllers/api/v1/alerts_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::AlertsController < ApplicationController
+	before_action :authenticate_with_token!, only: [:create]
 	respond_to :json
 	def index
 		respond_with  Alert.within_school(params[:id])
@@ -6,4 +7,18 @@ class Api::V1::AlertsController < ApplicationController
 	def show
 		respond_with Alert.find(params[:id])
 	end
+	def create
+		alert = Alert.new(alert_params)
+		alert.user_id = current_user.id
+		if alert.save
+			render json: alert, status: 201, location: [:api, alert]
+		else
+			render json: { errors: alert.errors }, status: 422
+		end
+	end
+	private
+
+    def alert_params
+      params.require(:alert).permit(:latitude, :longitude, :status, :admin_description, :user_description)
+    end
 end
--- app/models/alert.rb
+++ app/models/alert.rb
@@ -5,6 +5,9 @@ class Alert
   belongs_to :user
   belongs_to :school
 
+  before_save :add_path
+  before_validation :is_covered, on: :create
+
   scope :within_school, ->(school) {where(:school_id => school)}
 
   field :latitude, type: Float
@@ -12,4 +15,49 @@ class Alert
   field :status, type: String
   field :admin_description, type: String
   field :user_description, type: String
+  field :alert_path, type: Array
+
+  def haversine_distance(lat1, lng1, lat2, lng2)
+
+  	dtor = Math::PI/180
+    r = 3959
+
+    rlat1 = lat1 * dtor 
+    rlong1 = lng1 * dtor 
+    rlat2 = lat2 * dtor 
+    rlong2 = lng2 * dtor
+    dlon = rlong1 - rlong2
+    dlat = rlat1 - rlat2 
+
+    a = Math::sin(dlat/2) ** 2 + Math::cos(rlat1) * Math::cos(rlat2) * Math::sin(dlon/2) ** 2
+    c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
+
+    distance = r * c
+
+  	return distance
+  end
+  def is_covered
+  	schools = School.all
+
+  	schools.each do |school|
+
+  		distance = haversine_distance(school.latitude, school.longitude, self.latitude, self.longitude)
+
+  		Rails.logger.info(distance)
+  		if distance < school.radius  
+  			self.school_id = school.id
+  			self.status = "active"
+  		return self 
+  		else
+  			self.errors.add(:error,"Not within school")
+  		end
+  	end
+  end
+  def add_path
+    path = []
+    array = []
+    array.push(self.latitude, self.longitude)
+    path.push(array)
+    self.push(alert_path: path)
+  end
 end
